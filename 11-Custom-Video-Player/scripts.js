c228a250531d0e6081026fb4f15261b186477c68
parker <parkerschellenberg@gmail.com>
2023-04-17 16:40:23 -0500

    11 my reworked solution

--- 11-Custom-Video-Player/scripts.js
+++ 11-Custom-Video-Player/scripts.js
@@ -1,4 +1,5 @@
 const video = document.querySelector('video');
+const progress = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress__filled');
 const playButton = document.querySelector('button[title="Toggle Play"]');
 const volumeInput = document.querySelector('input[name="volume"]');
@@ -10,20 +11,45 @@ const forwardButton = document.querySelector('button[data-skip="25"]');
 video.currentTime = 0.1;
 
 // Play/Pause
-playButton.addEventListener('click', () => {
+playButton.addEventListener('click', playOrPause);
+video.addEventListener('click', playOrPause);
+
+function playOrPause() {
   (video.paused === true)
     ? video.play()
     : video.pause();
-});
+}
+
+video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
+
+function updatePlayButton() {
+  (video.paused === true)
+    ? playButton.textContent = '►'
+    : playButton.textContent = '❚❚';
+}
 
 // update Progress Bar
 video.addEventListener('timeupdate', () => {
   progressBar.style.flex = video.currentTime / video.duration;
 });
 
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+// progress.addEventListener('mouseout')
+
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 // Volume
 volumeInput.addEventListener('input', () => {
-  video.volume = volumeInput.value
+  video.volume = volumeInput.value;
 });
 
 // PlaybackRate
